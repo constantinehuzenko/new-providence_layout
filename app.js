5a14174e0e794e520debf83b5842c0de071dc7c1
Constantine Huzenko <kossguzenko@gmail.com>
2020-05-01 23:08:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 // Hamburger icon close animation
 var burgerClose = document.getElementById('burger')
+var burgerBody = document.getElementById('body')
+var burgerPage = document.getElementById('burger_page')
+var burgerHeader = document.getElementById('header')
 
 burgerClose.addEventListener('click', function () {
     burgerClose.classList.toggle('close');
+    burgerBody.classList.toggle('body_fix');
+    burgerPage.classList.toggle('burger_page_open');
+    burgerHeader.classList.toggle('white');
 });
 
 // Sticky nav bar
@@ -11,9 +17,9 @@ window.onscroll = function () { myFunction() };
 function myFunction() {
     if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
         document.getElementById("header").className = "header header_fixed";
-        // document.getElementById("jsmaincontent").className = "main-content fixmain";
+        document.getElementById("main_section").className = "main_section margin";
     } else {
         document.getElementById("header").className = "header";
-        // document.getElementById("jsmaincontent").className = "main-content";
+        document.getElementById("main_section").className = "main_section";
     }
 };
